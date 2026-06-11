410945711d8ccd4c489cda2a45fadc644baee2a3
Vaishnavi <vaishnavikambleraju@gmail.com>
2026-06-11 12:42:28 +0530

    random number generator

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+public class Main{
+    public static void main(String[] args){
+        Random random=new Random();
+        int num;
+        num=random.nextInt(1,6);
+        System.out.println(num);
+
+    }
+}
